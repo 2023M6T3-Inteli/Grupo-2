12193e1dd6a19e217afb98f0e0c936bae2c26e8a
pingu01 <99196159+pingu01@users.noreply.github.com>
2023-05-04 17:47:15 -0300

    button component

---
+++ codigo/frontend/components/cardproject/index.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export function CardProject(){
+    return (
+    <View>
+        <Text>Teste de cardproject</Text>
+    </View>
+    )
+}
---
+++ codigo/frontend/pages/Feedprojects/index.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {CardProject} from '../../components/cardproject';
+import styles from './style'
+
+export function Projects() {
+    return (
+      <View style={styles.container}>
+        <Text>Test 145222545</Text>
+        <Text>Teste de cardproject</Text>
+
+        <CardProject/>
+        
+        <StatusBar style="auto" />
+      </View>
+    );
+}
+
