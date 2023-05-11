5bd4d56182c5b2ed8222196c70a8188dfcde2d86
Gabriel Nascimento <99196159+pingu01@users.noreply.github.com>
2023-05-10 22:13:55 -0300

    health path

--- codigo/backend/index.js
+++ codigo/backend/index.js
@@ -1,13 +1,19 @@
 import express from "express"
 const app = express()
 
+//health check
+import {health} from "./entities/health/health.js"
+
 import {user} from "./entities/user/user.js"
 
-app.get("/",(req,res)=>{
-    res.send("Hello wordl")
-})
+
+app.use('/health' , health.loadRoutes)
+
+
 
 app.use('/user' , user.loadRoutes)
+
+
 
 app.listen(3000,()=>{
     console.log("Running on http://localhost:3000")
