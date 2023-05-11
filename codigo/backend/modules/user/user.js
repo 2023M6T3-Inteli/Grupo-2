da12898c46b43b991b18a022457951eac55dc0ab
pingu01 <99196159+pingu01@users.noreply.github.com>
2023-05-11 18:36:08 -0300

    get post endpoint

--- codigo/backend/modules/user/user.js
+++ codigo/backend/modules/user/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import {controllerGetProjects} from './getProjects/controller.js';
+import { controllerGetPosts } from './getPosts/controller.js';
 
 // create a class User with a router / with res send hello world
 class User {
@@ -10,15 +11,12 @@ class User {
     }
 
     get loadRoutes() {
-
-        this.router.get("/getProject",controllerGetProjects.exec )
-
-        this.router.get("/test2", (req, res) => {
-            res.send("Hello world 2");
-        });
+        this.router.get("/getProject",controllerGetProjects.exec );
+        this.router.get("/getPosts",controllerGetPosts.exec );
 
         return this.router;
     }
+
 }
 
 export const user = new User();
