a5d225c0396ee1ec145ead8f2f99f6284760bfc9
pingu01 <99196159+pingu01@users.noreply.github.com>
2023-05-12 10:44:03 -0300

    FIX

--- codigo/backend/modules/user/getPosts/services.js
+++ codigo/backend/modules/user/getPosts/services.js
@@ -2,7 +2,7 @@ import { prisma } from "../../../database/connection.js";
 
 class ServiceGetPosts {
         async exec() {
-            const projects = await prisma.project.findMany();
+            const projects = await prisma.post.findMany();
             return projects;
         }
     }
