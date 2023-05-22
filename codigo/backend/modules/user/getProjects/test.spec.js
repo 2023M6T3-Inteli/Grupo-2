ec72af68ea61988ab691a0f0cbe0bd33d926e7e8
pingu01 <99196159+pingu01@users.noreply.github.com>
2023-05-22 17:54:39 -0300

    some tests

---
+++ codigo/backend/modules/user/getProjects/test.spec.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+import { serviceGetProjects } from "./services.js";
+import { prisma } from "../../../database/connection.js";
+
+// describe("Post", () => {
+//     it("should return all posts !", async () => {
+//         await prisma.
+//     })
+// })
