1447979b0e38cc0f799b60b0d4d1f5d7e173197d
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2023-05-24 16:12:44 -0300

    FIX

--- codigo/backend/modules/user/Ranking/controller.js
+++ codigo/backend/modules/user/Ranking/controller.js
@@ -2,14 +2,27 @@ import { servicegetRanking } from "./service.js";
 
 
 class ControllergetRanking {
-    async exec(req, res){
+    async exec(req, res) {
 
-        const {id} = req.params
+        const { id } = req.params
 
-        const rankUsers = await servicegetRanking.exec(id);
-        res.send(rankUsers);
+        const userExist = await servicegetRanking.verifyUserById(Number(id))
+
+        if (!userExist) {
+            return res.status(400).send("Doesn't exist")
+        }
+
+
+        try {
+            const rankUsers = await servicegetRanking.exec(id);
+            return res.status(200).send(rankUsers);
+
+        } catch (error) {
+            return res.status(400).send(error)
+        }
+
     }
-    
+
 }
 
 export const controllergetRanking = new ControllergetRanking();
