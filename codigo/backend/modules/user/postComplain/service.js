51f189e265876f3217f0eb33db352766e5bdd5a2
mikemouadeb <mike.mouadeb@sou.inteli.edu.br>
2023-05-30 17:36:06 -0300

    try catch fix

--- codigo/backend/modules/user/postComplain/service.js
+++ codigo/backend/modules/user/postComplain/service.js
@@ -1,15 +1,20 @@
 import { prisma } from "../../../database/connection.js";
 
 class ServicePostComplain {
-    async exec(data) {
-        const response = await prisma.postComplaint.create ({
-            data: {
-                complaintReason: data.complaintReason,
-                feedback: data.feedback,
-                idPost: data.idPost,
-            }
-        });
-        return response;
+    async exec(data) { 
+        try {
+            const response = await prisma.postComplaint.create ({
+                data: {
+                    complaintReason: data.complaintReason,
+                    feedback: data.feedback,
+                    idPost: data.idPost,
+                }
+            });
+            return response;
+            
+        } catch (error) {
+            return error;
+        }
     }
 };
 
