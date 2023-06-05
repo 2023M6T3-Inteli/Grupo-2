dc785f4fa493d4fad067abb1ae0b06a0e5aba69a
pingu01 <99196159+pingu01@users.noreply.github.com>
2023-06-05 17:42:37 -0300

    logs installed but not working

--- codigo/backend/index.js
+++ codigo/backend/index.js
@@ -1,19 +1,33 @@
 import express from "express"
+import { Log4js } from "log4js"
 
-import swaggerJsdoc from "swagger-jsdoc"
-import swaggerUi from "swagger-ui-express"
+import {health} from "./modules/health/health.js"
+
+import {user} from "./modules/user/user.js"
 
 const app = express()
 // const bodyParser = require("body-parser")
 // const swaggerJsdoc = require("swagger-jsdoc")
 // const swaggerUi = require("swagger-ui-express");
 
+//configuring logger
+Log4js.configure({
+    appenders: {
+      multi:{
+        type: "multiFile",
+        base: "logs/",
+        property: "categoryName",
+        extension: ".log",
+      },
+    },
+    categories: {
+      default: { appenders: ["multi"], level: "debug"},
+    },
+});
+
+
+
 app.use(express.json())
-
-//health check
-import {health} from "./modules/health/health.js"
-
-import {user} from "./modules/user/user.js"
 
 app.use('/health' , health.loadRoutes)
 
@@ -23,32 +37,6 @@ app.listen(3000,()=>{
     console.log("Running on http://localhost:3000")
 })
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "LogRocket Express API with Swagger",
-      version: "0.1.0",
-      description:
-        "This is a simple CRUD API application made with Express and documented with Swagger",
-      license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
-      },
-      contact: {
-        name: "LogRocket",
-        url: "https://logrocket.com",
-        email: "info@email.com",
-      },
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
 
 const specs = swaggerJsdoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
